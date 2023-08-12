d7e3818043467d997cc7d8b8b688fb8b51cd2a33
Flauton <113304041+FlautonGT@users.noreply.github.com>
2023-08-12 23:09:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const dailyWords = [
-  "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
-  "The only way to do great work is to love what you do.",
-  "Success is not final, failure is not fatal: It is the courage to continue that counts.",
-  "In the middle of every difficulty lies opportunity.",
-  "Your time is limited, don't waste it living someone else's life.",
+  {
+    text: "Believe in yourself and all that you are. Know that there is something inside you that is greater than any obstacle.",
+    author: "Unknown"
+  },
+  {
+    text: "The only way to do great work is to love what you do.",
+    author: "Steve Jobs"
+  },
+  {
+    text: "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+    author: "Winston Churchill"
+  },
+  {
+    text: "In the middle of every difficulty lies opportunity.",
+    author: "Albert Einstein"
+  },
+  {
+    text: "Your time is limited, don't waste it living someone else's life.",
+    author: "Steve Jobs"
+  },
   // Add more daily words here
 ];
 
@@ -12,27 +27,32 @@ function getRandomWord() {
   return dailyWords[randomIndex];
 }
 
-function updateDailyWord() {
+function updateDailyWord(word) {
   const dailyWordElement = document.getElementById("dailyWord");
-  dailyWordElement.textContent = getRandomWord();
+  dailyWordElement.innerHTML = `<em>"${word.text}"</em> - <strong>${word.author}</strong>`;
+}
+
+function refreshWord() {
+  const randomWord = getRandomWord();
+  updateDailyWord(randomWord);
 }
 
 function sendWhatsAppMessage() {
   const word = getRandomWord();
-  const message = `Hello! Please send me the daily word. My email: [Your Email]`;
-
+  const message = `Hello! Please send me the word: "${word.text}" by ${word.author}. My email: [Your Email]`;
+  const encodedMessage = encodeURIComponent(message);
+  
   // Update the WhatsApp link to include the message
   const whatsappLink = document.getElementById("whatsappLink");
-  whatsappLink.href = `https://wa.me/6283817424428/?text=${encodeURIComponent(message)}`;
+  whatsappLink.href = `https://wa.me/whatsappphonenumber/?text=${encodedMessage}`;
 }
 
 // Initial setup
-updateDailyWord();
-setInterval(updateDailyWord, 3600000); // Update every 1 hour (in milliseconds)
-
-// Set up the Refresh button
 const refreshButton = document.getElementById("refreshButton");
-refreshButton.addEventListener("click", updateDailyWord);
+refreshButton.addEventListener("click", refreshWord);
+
+updateDailyWord(getRandomWord());
+setInterval(refreshWord, 3600000); // Update every 1 hour (in milliseconds)
 
 // Set up the WhatsApp link
 const whatsappLink = document.getElementById("whatsappLink");
